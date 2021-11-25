c64638451df0151200544b0a79e426d648ed3165
Charlie <boy_916@hotmail.com>
2021-11-24 22:08:57 -0800

    finished lab

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # serverless-api
+
+In our Serverless API, we are creating a few things that will all be run from RESTful API's. Once our RESTful API triggers, we will make a request to our serverless API. Then our lambda function will run which will connect us to our database to store our data in our tables we made in Dynamoose.
+
+## DynamoDB
+
+We use DynamoDB we use tables that store our data in them. They work off of our schema to create our database.
+
+## API Gateway
+
+Here we create our RESTful API's which include not limited too, GET, POST, PUT, and DELETE. We can create the pathway for our API's and even set parameters or deeper layers in our API all in AWS API Gateway.
+
+## Questions
+
+- What is the root URL to your API?
+  - arn:aws:execute-api:us-west-2:850427120219:8mxpme0xz3/*/GET/people
+- What are the routes?
+  - people
+- What inputs do they require?
+  - name
+- What output do they return?
+  - id and name
+
+
+## contributors
+
+- Ayrat Gimranov
+- Mark Thanadabouth
+- Tom McGuire
--- index.js
+++ index.js
@@ -1,35 +1,99 @@
 'use strict';
 
+let uuid = require('uuid').v4;
 const peopleModel = require('./people.schema');
 
 exports.handler = async (event) => {
 
-  console.log('EVENT', event);
+  if(event.httpMethod === 'POST') {
+    try {
+      let { name } = JSON.parse(event.body);
+      let id = uuid();
 
-  try {
+      let record = new peopleModel({ id, name});
 
-    // either there is or is not an id
-    const id = event.pathParameters && event.pathParameters.id;
+      const data = await record.save();
 
-    let data;
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+      };
+    } catch (err) {
+      return {
+        statusCode: 500,
+        response: err.message
+      };
+    };
+  } else if (event.httpMethod === 'GET') {
 
-    if (id) {
-      const list = await peopleModel.query('id').eq(id).exec();
-      console.log('LIST', list);
-      data = list[0];
-    } else {
-      data = await peopleModel.scan().exec();
-    }
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    }
+    // Taken from Solution Code
+    try {
+  
+      // either there is or is not an id
+      const id = event.pathParameters && event.pathParameters.id;
+  
+      let data;
+  
+      if (id) {
+        const list = await peopleModel.query('id').eq(id).exec();
+        data = list[0];
+      } else {
+        data = await peopleModel.scan().exec();
+      };
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+  
+    } catch (err) {
+      return {
+        statusCode: 500,
+        response: err.message
+      };
+    };
 
-  } catch (err) {
-    return {
-      statusCode: 500,
-      response: err.message
-    }
-  }
+    // End solution Code
+  } else if (event.httpMethod === 'PUT') {
+    try {
+      let { name } = JSON.parse(event.body);
 
-}
+      // If first is true, use the second one
+      // if first is false, return null
+      let id = event.pathParameters && event.pathParameters.id;
+
+      let peopleUpdate;
+
+      if(id) {
+        peopleUpdate = await peopleModel.update({ 'id': id, 'name': name});
+      };
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+    } catch (err) {
+      return {
+        statusCode: 500,
+        response: err.message
+      };
+    };
+  } else if (event.httpMethod === 'DELETE') {
+    try {
+
+      let id = event.pathParameters && event.pathParameters.id;
+
+      if(id) {
+        await peopleModel.delete({'id': id});
+      };
+
+      return {
+        statusCode: 200,
+        body: 'Deleted Name'
+      };
+    } catch (err) {
+      return {
+        statusCode: 500,
+        response: err.message
+      };
+    };
+  };
+};
